dc6921c07234776020923193a07a43d8294311ab
NwebMyosotis <dynamickid@naver.com>
2022-10-05 19:59:50 +0900

    learned edit page, more schema, error controller
    schema type,
    error controller
    regular expression(hexadeciaml)
    findOne(), findById() ~ findByIdAndUpdate()

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -8,8 +8,8 @@ import {
 } from "../controllers/videoController.js";
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
