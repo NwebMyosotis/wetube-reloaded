640b515110e68b3a0e779bb4b16b60074edac9d1
NwebMyosotis <dynamickid@naver.com>
2022-10-18 21:11:48 +0900

    add session #1
    1. express-session
    2. app.use(session({...)}
    3. req.sessionStorage.all
    4. req.session
    5. login session, use session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,6 +58,8 @@ export const postLogin = async (req, res) => {
     });
   }
   console.log("🆗 LOG USER IN! COMMING SOON!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 // const express = require("express"); 와 같으나 위에가 더 최신 문법임.
 // node_modules의 "express"에서 express패키지를 찾아 파일에서 활용할 수 있도록 가져옴. 즉 from "express"는 정확히 말하면 node_modules/express임
 import morgan from "morgan";
@@ -15,6 +16,27 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.test += 1;
+  return res.send(`${req.session.id}\n${req.session.test}`);
+});
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
