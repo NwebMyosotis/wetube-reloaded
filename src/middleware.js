e7670d1d56f996660ede8c58bd1a4a2ae6b42a8d
NwebMyosotis <dynamickid@naver.com>
2022-10-18 23:04:34 +0900

    add session #2
    learned using locals

---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user;
+  console.log(res.locals);
+  next();
+};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import session from "express-session";
 // const express = require("express"); 와 같으나 위에가 더 최신 문법임.
 // node_modules의 "express"에서 express패키지를 찾아 파일에서 활용할 수 있도록 가져옴. 즉 from "express"는 정확히 말하면 node_modules/express임
 import morgan from "morgan";
+import { localsMiddleware } from "./middleware.js";
 import rootRouter from "./routers/rootRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
@@ -25,18 +26,7 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
-
-app.get("/add-one", (req, res, next) => {
-  req.session.test += 1;
-  return res.send(`${req.session.id}\n${req.session.test}`);
-});
-
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
