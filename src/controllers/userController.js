9d0b5fd3eca3cb47aa273840cc4672c5ccd982c0
NwebMyosotis <dynamickid@naver.com>
2022-10-21 17:25:48 +0900

    adding github login
    임시저장

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import fetch from "cross-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
@@ -61,6 +62,52 @@ export const postLogin = async (req, res) => {
   req.session.loggedIn = true;
   req.session.user = user;
   return res.redirect("/");
+};
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      //fetch는 url에서 정보를 읽어들이는 함수
+      method: "POST", // 기본값은 GET이고, GET이면 url로 부터 콘텐츠가 다운로드 됨
+      headers: {
+        Accept: "application/json", // 이 부분이 없으면 github가 text로 응답함
+      },
+    })
+  ).json();
+  // const json = await data.json(); //fetch는 단독으로는 사용할 수 없고 응답을 텍스트나 json 등으로 변환해야함. 여기서는 json.
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
 };
 
 export const edit = (req, res) => res.send("Edit User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController.js";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -7,5 +14,7 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get(":/id", see);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
