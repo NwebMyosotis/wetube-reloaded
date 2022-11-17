4f67d2e23f064a0ef12767e2976a6b9a80916855
NwebMyosotis <dynamickid@naver.com>
2022-11-17 20:56:05 +0900

    Webpack #2

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,2 @@
 import "../scss/styles.scss";
-
-console.log("Hi");
+alert("It's working");
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
+  watch: true,
   entry: "./src/client/js/main.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
@@ -20,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.scss/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
