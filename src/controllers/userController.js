1da33e3ef9a7ab0088f5f81f1cbde165feec9c8c
NwebMyosotis <dynamickid@naver.com>
2022-11-30 22:06:34 +0900

    scss change: ver.Nico

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -305,7 +305,13 @@ export const logout = (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User Not Found" });
   }
