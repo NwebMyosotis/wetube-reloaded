bbc5f1488f4d955d3d592f7cbaf4792c77948454
NwebMyosotis <dynamickid@naver.com>
2022-12-21 18:22:55 +0900

    videoRecorder #1

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,2 @@
+import regeneratorRuntime from "regenerator-runtime";
 import "../scss/styles.scss";
-console.log("Main");
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   },
   plugins: [
     new MiniCssExtractPlugin({
