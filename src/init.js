2022bb7e5e7c26fa4c9590c0b68922495cd5db21
NwebMyosotis <dynamickid@naver.com>
2023-02-01 18:00:15 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import User from "./models/User.js";
 import Comment from "./models/Comment.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
